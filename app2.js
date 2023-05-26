2376a0fd1e57305cf20d44b4c9992ab6beec86d1
fuad <atandafuad35@gmail.com>
2023-05-26 11:21:39 +0100

    Error corrected

--- app2.js
+++ app2.js
@@ -1,27 +1,27 @@
-// Increasing and Decreasing counter
-let count = 0
-const value = document.getElementById('value')
-const btns = document.querySelectorAll('.btn')
+let counts = 0
+
+const value = document.querySelector("#value")
+const btns = document.querySelectorAll(".btn")
 
 btns.forEach(function (btn) {
-  btn.addEventListener('click', (e) => {
-    const styles = e.currentTarget.classlist
-    if(styles.contains('decrease')) {
-      count--
-    } else if (styles.contains('increase')) {
-      count++
-    } else {
-      count = 0
-    }
-    if (count > 0) {
-      value.style.color = 'green'
-    }
-     if (counts < 0) {
+    btn.addEventListener('click', (e) => {
+        const styles = e.currentTarget.classList
+        if(styles.contains('decrease')) {
+            counts--
+        } else if (styles.contains('increase')) {
+            counts++
+        } else {
+            counts = 0
+        }
+        if (counts > 0) {
+            value.style.color = 'green'
+        }
+        if (counts < 0) {
             value.style.color = 'red'
         }
         if (counts === 0) {
             value.style.color = '#222'
         }
-    value.textContent = count
-  })
+        value.textContent = counts
+    })
 })
