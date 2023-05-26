e48c7fd6dc107d9af756a7bc26015174e51b64c0
fuad <atandafuad35@gmail.com>
2023-05-26 10:17:50 +0100

    2nd commit

--- app.js
+++ app.js
@@ -6,3 +6,19 @@ const saveBtn = document.getElementById('save-btn')
 console.log(saveEl)
 
 let count = 0
+
+incrementBtn.addEventListener('click', increment)
+saveBtn.addEventListener('click', save)
+
+function increment () {
+  count += 1
+  countEl.textContent = count
+  console.log(count)
+}
+
+function save () {
+  let countStr = count + " - "
+  saveEl.textContent += countStr
+  countEl.textContent = 0
+  count = 0
+}
