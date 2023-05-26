27e7556c4c09ec8d40eff56315011b021c91af44
fuad <atandafuad35@gmail.com>
2023-05-26 10:03:47 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+let saveEl = document.getElementById('save-el')
+let countEl = document.getElementById('count-el')
+const incrementBtn = document.getElementById('increment-btn')
+const saveBtn = document.getElementById('save-btn')
+
+console.log(saveEl)
+
+let count = 0
